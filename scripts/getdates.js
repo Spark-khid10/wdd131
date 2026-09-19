a4488cc3020f51cfb149866ed124c9d32f4df2b9
Ephraim Otikor <eotikor@byupathway.edu>
2026-09-19 17:19:50 +0100

    Complete W01 home page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Current year for the footer copyright
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Date the document was last modified
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
